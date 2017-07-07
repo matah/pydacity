e4cf3d9b8747d2e0ef78b0bad483330be8a96d38
Selma Ardelean <selma.ardelean@gmail.com>
2017-07-07 14:19:15 +0100

    renamed license file and added a setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pydacity',
+  packages = ['pydacity'], # this must be the same as the name above
+  version = '0.1.0',
+  description = 'Wrapper around ffmpeg',
+  author = 'Selma Ardelean',
+  author_email = 'selma.ardelean@gmail.com',
+  url = 'https://github.com/matah/pydacity', # use the URL to the github repo
+  download_url = 'https://github.com/matah/pydacity/archive/0.1.0.tar.gz', # I'll explain this in a second
+  keywords = ['ffmpeg', 'audio'], # arbitrary keywords
+  classifiers = [],
+)
